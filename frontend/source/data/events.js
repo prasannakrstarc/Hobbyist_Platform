7662a78e198a8fa90ac1152c12b3335af5872590
prasannakrstarc <mahalu.1971@gmail.com>
2025-06-12 18:05:14 +0530

    Create events.js

---
+++ frontend/source/data/events.js
@@ -0,0 +1,18 @@
+const events = [
+  {
+    id: 1,
+    title: "React Meetup",
+    date: "2025-06-20",
+    location: "Bangalore",
+    description: "A community meetup for React enthusiasts."
+  },
+  {
+    id: 2,
+    title: "Hobby Coding Jam",
+    date: "2025-06-25",
+    location: "Online",
+    description: "Collaborate with hobbyists and build mini-projects together!"
+  }
+];
+
+export default events;
